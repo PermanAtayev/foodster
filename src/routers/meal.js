275221c56292ceefff16ae581579f515a3508860
Perman <perman.atayev17@gmail.com>
2020-12-27 13:31:47 +0300

    generate random meal is improved, deployment database is updated #4

--- src/routers/meal.js
+++ src/routers/meal.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const db = require('../_helpers/db');
 const User = require('../mongo/model/user');
+const Recipe = require('../mongo/model/recipe');
 const auth = require('../middleware/auth')
 
-router.get('/meals/generate', auth, async(req, res) => {
-    const random_meal = {
-        "name" : "somsa",
-        "cookingTime" : 50
-    }
-    res.send(random_meal);
+router.get('/meals/generateRandom', auth, async(req, res) => {
+    const n = Recipe.count({});
+    let r = Math.floor(Math.random() * n);
+    let randomMeal = await Recipe.find({}).limit(1).skip(r);
+
+    res.status(200).send(randomMeal);
 })
 
 module.exports = router;
