f689f6dbb0bebe58d30425ad420fd02fdadf4b8a
ibrahim elmas <ibrahimelmas.3452@gmail.com>
2021-04-06 16:53:09 +0300

    some endpoints are tested now

--- __tests__/recipe_router.test.js
+++ __tests__/recipe_router.test.js
@@ -1,39 +1,62 @@
 const request = require('supertest');
 const app = require('../src/app');
 const mongoose = require('mongoose');
+const Recipe = require('./../src/mongo/model/Recipe');
 
-const {test_user} = require('../src/_helpers/test_helpers');
+
+const {test_user, test_recipe, test_recipe2} = require('../src/_helpers/test_helpers');
 
 let token = null;
 
 beforeEach(async(done) => {
     if (mongoose.connection.readyState !== 1){
         await mongoose.connect(process.env.CONNECTION_STRING_TEST,
-        { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-        async() => {
-            const res = await request(app)
+        { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+        const res = await request(app)
                 .post('/users/login')
                 .send({
                     email: test_user.email,
                     password: test_user.password,
                 });
-            token = 'Bearer ' + res.body.token;
-            done();
-        });
+        token = 'Bearer ' + res.body.token;
+        
+        done();
     }
 });
-afterEach((done) => {
-    mongoose.connection.close(() => done());
+afterEach(async(done) => {
+    await mongoose.connection.close();
+    done();
 });
 
 
 test('Should get recipe', async (done) => {
+    const res = await request(app)
+        .post('/recipes/addRecipe')
+        .set('Authorization', token) //set header for this test
+        .send(test_recipe);
     const res2 = await request(app)
-        .post('/recipe/nameFilter')
+        .post('/recipes/nameFilter')
         .set('Authorization', token) //set header for this test
         .send({
-            recipe_name: "Fabulous Fruit Salad"
+            recipe_name: test_recipe.name
         });
+    const recipe = await Recipe.findOne({name: test_recipe.name});
+    await recipe.delete();
     expect(res2.statusCode).toEqual(201);
+    done();
+});
+
+test('Should add recipe', async (done) => {
+    const res2 = await request(app)
+        .post('/recipes/addRecipe')
+        .set('Authorization', token) //set header for this test
+        .send(test_recipe2);
+    const recipe = await Recipe.findOne({name: test_recipe2.name});
+    if (recipe)
+        await Recipe.deleteOne({name: test_recipe2.name});
+    const recipeName = recipe.name;
+    expect(res2.statusCode).toEqual(201);
+    expect(recipe).toEqual(expect.anything());
+    expect(recipeName).toEqual(test_recipe2.name);
     done();
 });
--- __tests__/user_router.test.js
+++ __tests__/user_router.test.js
@@ -1,15 +1,27 @@
 const request = require('supertest');
 const app = require('../src/app');
 const mongoose = require('mongoose');
-const {test_user} = require('../src/_helpers/test_helpers');
+const {test_user, test_recipe} = require('../src/_helpers/test_helpers');
+const User = require('./../src/mongo/model/User');
+const Recipe = require('../src/mongo/model/recipe');
+
+let token = null;
 
 beforeEach(async(done) => {
     if (mongoose.connection.readyState !== 1){
         await mongoose.connect(process.env.CONNECTION_STRING_TEST,
-        { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-        () => done());
+        { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+        const res = await request(app)
+                .post('/users/login')
+                .send({
+                    email: test_user.email,
+                    password: test_user.password,
+                });
+        token = 'Bearer ' + res.body.token;
+        done();
     }
 });
+
 afterEach((done) => {
     mongoose.connection.close(() => done());
 });
@@ -25,5 +37,71 @@ test('Should login', async (done) => {
     // Assert that the database was changed correctly
     //console.log(res.body);
     expect(res.statusCode).toEqual(200);
+    done();
+});
+
+test('Should update information of user', async (done) => {
+    const res2 = await request(app)
+        .post('/users/updateInfo')
+        .set('Authorization', token)
+        .send({
+            age: 25,
+            gender: "Helicopter"
+        });
+    const user = await User.findOne({email: test_user.email});
+    expect(user).toEqual(expect.anything());
+    if (user){
+        user.age = 18;
+        user.gender = "TestDocument";
+        await user.save();
+    }
+    expect(res2.statusCode).toEqual(201);
+    done();
+});
+
+test('Should find the liked meals', async (done) => {
+    const res = await request(app)
+        .get('/users/myLikedRecipes')
+        .set('Authorization', token)
+        .send({
+            email: test_user.email
+        });
+    
+    expect(res.statusCode).toEqual(200);
+    const user = await User.findOne({email: test_user.email});
+    const populatedUser = await user.populate("likedRecipes", "name").execPopulate();
+    expect(res.body.length).toEqual(populatedUser.likedRecipes.length);
+    expect(JSON.stringify( res.body[0])).toStrictEqual(JSON.stringify( populatedUser.likedRecipes[0]));
+    done();
+});
+
+test('Should like the meal', async (done) => {
+    const res = await request(app)
+        .post('/recipes/addRecipe')
+        .set('Authorization', token) //set header for this test
+        .send(test_recipe);
+    expect(res.statusCode).toEqual(201);
+
+    const res2 = await request(app)
+        .post('/users/likeRecipe')
+        .set('Authorization', token)
+        .send({
+            email: test_user.email,
+            recipeName: test_recipe.name
+        });
+    expect(res2.statusCode).toEqual(200);
+    const user = await User.findOne({email: test_user.email});
+    const recipe = await Recipe.findOne({name: test_recipe.name});
+    expect(user).toEqual(expect.anything());
+    expect(recipe).toEqual(expect.anything());
+    if (user && recipe){
+        expect(user.likedRecipes.includes(recipe._id)).toEqual(true);
+        const index = user.likedRecipes.indexOf(recipe._id);
+        if (index > -1) {
+            await user.likedRecipes.splice(index, 1);
+        }
+        await user.save();
+    }
+    if (recipe) await Recipe.deleteOne({name: recipe.name});
     done();
 });
