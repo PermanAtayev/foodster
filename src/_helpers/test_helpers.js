099ca2c78ffa49beab4047cdaa50371caa168c47
ibrahim elmas <ibrahimelmas.3452@gmail.com>
2021-04-11 03:13:27 +0300

    weird bug in testing is solved

--- src/_helpers/test_helpers.js
+++ src/_helpers/test_helpers.js
@@ -11,30 +11,23 @@ const test_recipe = {
     "instructions": ["Whisk vinegar, soy sauce, garlic, olive oil, brown sugar, and crushed peppercorns in a bowl until sugar is dissolved. Stir bay leaves into the sauce"],
     "likedUsers": [],
     "type": "breakfast",
-    "name": "A",
+    "name": "D",
     "prepTime": 5,
     "cookTime": 35,
-    "ingredients": [
-        {
-            "text": "1 tbsp of sugar and a lot of salt",
-            "unit": "tbsp",
-            "name": "pepper",
-            "originID": "604533cedd23980b2203f017"
-        }
-    ],
+    "ingredients": [],
     "__v": 0
 };
 
-    const test_recipe2 = {
-        "imgPath": "",
-        "servingSize": 4,
-        "instructions": ["Whisk vinegar, soy sauce, garlic, olive oil, brown sugar, and crushed peppercorns in a bowl until sugar is dissolved. Stir bay leaves into the sauce"],
-        "likedUsers": [],
-        "type": "breakfast",
-        "name": "B",
-        "prepTime": 5,
-        "cookTime": 35,
-        "ingredients": [],
-        "__v": 0};
+const test_recipe2 = {
+    "imgPath": "",
+    "servingSize": 4,
+    "instructions": ["Whisk vinegar, soy sauce, garlic, olive oil, brown sugar, and crushed peppercorns in a bowl until sugar is dissolved. Stir bay leaves into the sauce"],
+    "likedUsers": [],
+    "type": "breakfast",
+    "name": "E",
+    "prepTime": 5,
+    "cookTime": 35,
+    "ingredients": [],
+    "__v": 0};
 
 module.exports = {test_user, test_recipe, test_recipe2};
